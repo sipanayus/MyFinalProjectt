c68d3cd5ba4e8dd153c0c800d14fe45823a11f87
sipan <sipan@MY-BOOK>
2021-02-27 02:09:41 +0300

    WorkShop

--- Business/ValidationRules/FluentValidation/ProductValidator.cs
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace Business.ValidationRules.FluentValidation
 {
-    public class ProductValidator:AbstractValidator<Product>
+    public class ProductValidator : AbstractValidator<Product>
     {
         public ProductValidator()
         {
@@ -14,8 +14,9 @@ namespace Business.ValidationRules.FluentValidation
             RuleFor(p=>p.ProductName).MinimumLength(2);
             RuleFor(p => p.UnitPrice).NotEmpty();
             RuleFor(p => p.UnitPrice).GreaterThan(0);
-            RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1);
             RuleFor(p => p.ProductName).Must(StartWithA).WithMessage("Ürünler A harfi ile başlamalı");
+            RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1);
+           
         }
 
         private bool StartWithA(string arg)
